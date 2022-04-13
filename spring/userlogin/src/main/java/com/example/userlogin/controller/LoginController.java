f0e46095868452fb2647d06b3a77c3f0bb5ea79e
raash-22 <raashmip22@gmail.com>
2022-04-13 11:53:01 +0530

    signup login backend

---
+++ spring/userlogin/src/main/java/com/example/userlogin/controller/LoginController.java
@@ -0,0 +1,32 @@
+package com.example.userlogin.controller;
+
+import com.example.userlogin.model.LoginModel;
+import com.example.userlogin.model.UserModel;
+import com.example.userlogin.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/login")
+public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginModel>checkUser(@RequestBody LoginModel loginModel){
+
+        LoginModel loginModel1=loginService.findByEmail(loginModel.getEmail());
+        if(loginModel1==null){
+            return new ResponseEntity<LoginModel>(HttpStatus.NOT_FOUND);
+        }
+        if(!loginModel1.getPassword().equals(loginModel.getPassword())){
+            return new ResponseEntity<LoginModel>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<LoginModel>(HttpStatus.OK);
+    }
+
+}
