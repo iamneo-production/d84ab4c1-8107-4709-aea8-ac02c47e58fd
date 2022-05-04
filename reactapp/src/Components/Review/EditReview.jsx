17772c3d0e869059d4cec99e52d0976ac113068e
shyamofficial <shyamsundarpkofficial@gmail.com>
2022-05-04 09:57:04 +0530

    done

--- reactapp/src/Components/Review/EditReview.jsx
+++ reactapp/src/Components/Review/EditReview.jsx
@@ -86,6 +86,7 @@ export default function EditReview() {
         width: 900,
         height: 600,
         padding: 5,
+        marginLeft: 320,
         boxShadow:
           "rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px",
       }}
